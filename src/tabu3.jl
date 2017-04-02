b0b57a542cad2cf5fe6c279abc95c6fa67ab0e2e
afrometal <radek.kwlsk@gmail.com>
2017-04-02 09:54:53 +0200

    reformat, break condition reworked, new prints

--- src/tabu3.jl
+++ src/tabu3.jl
@@ -1,8 +1,22 @@
 using Distances, StatsBase
 
-dist = Euclidean()
-
 const DEBUG = length(ARGS) >=1 && ARGS[1] == "1"
+
+function distance(solution::Array{Int32})
+    cost = 0
+    for i in 1:length(solution)-1
+        cost += d[solution[i],solution[i+1]]
+    end
+    return cost
+end
+
+function istabbed(i::Integer, j::Integer, iteration::Integer)
+    return (iteration - tabulist[i]) < penalty || (iteration - tabulist[j]) < penalty
+end
+
+function isaspiring(i::Integer, j::Integer, current::Float64, base::Float64, itr::Integer)
+    return base > current + 0.7(penalty-(itr-min(tabulist[i], tabulist[j])))
+end
 
 tic()
 tic()
@@ -17,43 +31,20 @@ n = parse(Int32, n)
 # 2xN node's coordinates matrix
 # Data[:, k] = [x_k, y_k]
 data = Array{Float64}(2, n)
-# NxN node's distances matric
-d = Array{Float64}(n, n)
 
 # read file data to Data matrix
 while !done(input, state)
     (v, state) = next(input, state)
     (i_v, x_v, y_v) = split(v)
-    ii = parse(Int32, i_v)
-    xx = parse(Float64, x_v)
-    yy = parse(Float64, y_v)
-    data[:,ii] = [xx, yy]
+    data[:,parse(Int32, i_v)] = [parse(Float64, x_v), parse(Float64, y_v)]
 end
 # calculate nodes distances as NxN matrix
-pairwise!(d, dist, data)
 
-# show(d)
-
-function distance(solution::Array{Int32})
-    cost = 0
-    for i in 1:length(solution)-1
-        cost += d[solution[i],solution[i+1]]
-    end
-    return cost
-end
-
-frequencylist = zeros(Float64, n)
-frequencylist[1] = Inf
+d = Array{Float64}(n, n)
+pairwise!(d, Euclidean(1e-12), data)
 
 currentsol = Array{Int32}(n+1)
 currentsol[1] = currentsol[n+1] = Int32(1)
-bestsol = Array{Int32}(n+1)
-bestsol[1] = bestsol[n+1] = 1
-
-#
-# for i in 2:n
-#     currentsol[i] = Int32(i)
-# end
 
 for i in 1:n
     d[i,i] = Inf
@@ -78,27 +69,16 @@ bestcost = currentcost
 bestneighborsol = copy(currentsol)
 bestneighborcost = currentcost
 
-neighborsol = copy(bestneighborsol)
-neighborcost = bestneighborcost
-
-N = [(j, k) for j=2:length(neighborsol)-2 for k=j+1:length(neighborsol)-1]
+N = [(j, k) for j=2:length(bestsol)-2 for k=j+1:length(bestsol)-1 if k-j>1]
 
 neighborscount = length(N)
-
-function istabbed(i::Integer, j::Integer, iteration::Integer)
-    return (iteration - tabulist[i]) < penalty || (iteration - tabulist[j]) < penalty
-end
-
-function isaspiring(i::Integer, j::Integer, current::Float64, base::Float64, itr::Integer)
-    return base > current + 0.7(penalty-(itr-min(tabulist[i], tabulist[j])))
-end
 
 ####### SETTINGS #######
 const itermax = round(Int32, 1.0e8*round(sqrt(n))/n^2)
 
-const penalty = round(Int32, sqrt(n)) # rounds tabbed
+const penalty = 3*round(Int32, sqrt(n)) # rounds tabbed
 
-const neighborstocheck = round(Int32, sqrt(neighborscount) * 0.1)
+const neighborstocheck = round(Int32, sqrt(neighborscount)*0.7)
 ########################
 
 tabulist = fill(-penalty, n)
@@ -107,7 +87,9 @@ checks = 0
 if DEBUG
     println(string("max iterations:\t", itermax))
     println(string("initial cost:\t", bestcost))
-    # println(string("initial route: ", bestsol))
+    for n in bestsol
+        println(STDERR, n)
+    end
     println(string("initial time:\t", toq(), "s"))
 end
 
@@ -138,19 +120,16 @@ for iter in 1:itermax
 
             if !istabbed(city1, city2, iter) || isaspiring(city1, city2, currentcost, neighborcost, iter)
                 if currentcost <= bestneighborcost || index1 == 0
-                    if index1 != 0
-                        maxchecks = min(maxchecks, checks+0.05neighborstocheck)
-                    end
                     index1 = j
                     index2 = k
                     bestneighborcost = currentcost
+                    if currentcost <= bestcost
+                        maxchecks = min(maxchecks, checks+0.02neighborstocheck)
+                        # break
+                    end
                 end
                 checks += 1
             end
-        end
-
-        if checks >= maxchecks && index1 == 0
-            maxchecks += neighborstocheck
         end
 
         tabulist[city1] = iter
@@ -171,13 +150,13 @@ for iter in 1:itermax
 end
 
 if DEBUG
-    println(STDOUT, string("best cost:\t", bestcost))
+    println(string("best cost:\t", bestcost))
     println(string("total time:\t", toq(), "s"))
 else
     println(STDOUT, bestcost)
     for n in bestsol
-        print(STDERR, n)
-        print(STDERR, " ")
+        println(STDERR, n)
+        # print(STDERR, " ")
     end
-    println(STDERR)
+    # println(STDERR)
 end
